15c5c76525c2f12e5d0f716b4c47b834d82ae26b
Tommy Buado <tommy@thinkpad.local>
2023-07-06 23:56:21 +0800

    Add send_notification function in microphone.sh script

--- .local/bin/microphone.sh
+++ .local/bin/microphone.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 DEFAULT_SOURCE="@DEFAULT_AUDIO_SOURCE@"
 
+send_notification () {
+	STATUS="$(wpctl get-volume $DEFAULT_SOURCE | awk -F ' ' '{print $3}')"
+	VOLUME="$(wpctl get-volume $DEFAULT_SOURCE | awk -F ' ' '{print $2 * 100}')"
+
+	if [ ! -z "$STATUS" ] && [ "$STATUS" = "[MUTED]" ]; then
+		dunstify -i $HOME/.local/share/icons/dunst/switch-off.png \
+			-h string:x-dunst-stack-tag:microphone \
+			"Microphone Off"
+	else
+		dunstify -i $HOME/.local/share/icons/dunst/microphone.png \
+			-h string:x-dunst-stack-tag:microphone \
+			-h int:value:$VOLUME "Microphone Control"
+	fi
+}
+
 COMMAND=$1
 
 case $COMMAND in
 	toggle)
 		wpctl set-mute $DEFAULT_SOURCE toggle
+		send_notification
 		;;
 	down)
 		wpctl set-volume $DEFAULT_SOURCE 5%-
+		send_notification
 		;;
 	up)
 		wpctl set-volume $DEFAULT_SOURCE 5%+
+		send_notification
 		;;
 esac
