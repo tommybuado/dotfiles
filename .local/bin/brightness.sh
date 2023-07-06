b7262ad3dddb7dec68bfd1af498d4493d87e35d6
Tommy Buado <tommy@thinkpad.local>
2023-07-06 23:59:13 +0800

    Add send_notification function in brightness.sh script

--- .local/bin/brightness.sh
+++ .local/bin/brightness.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
+send_notification () {
+	BRIGHTNESS="$(brillo -b)"
+	dunstify -i $HOME/.local/share/icons/dunst/brightness.png \
+		-h string:x-dunst-stack-tag:brightness \
+		-h int:value:$BRIGHTNESS "Brightness Control"
+}
+
 COMMAND=$1
 
 case $COMMAND in
 	down)
 		brillo -U 5 > /dev/null
+		send_notification
 		;;
 	up)
 		brillo -A 5 > /dev/null
+		send_notification
 		;;
 esac
