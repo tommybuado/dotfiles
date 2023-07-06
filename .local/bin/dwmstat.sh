5781d53aa5de8f520391eb89e6636964fbf91db6
Tommy Buado <tommy@thinkpad.local>
2023-07-07 05:20:30 +0800

    Add print_weather function in dwmstat.sh script

--- .local/bin/dwmstat.sh
+++ .local/bin/dwmstat.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+print_weather () {
+	if [ ! -d "$HOME/.cache/wttr.in/" ]; then
+		mkdir -p $HOME/.cache/wttr.in/
+
+		if [ ! -f "$HOME/.cache/wttr.in/location" ]; then
+			touch $HOME/.cache/wttr.in/location
+		fi
+	fi
+
+	LOCATION="$(head -n 1 "$HOME/.cache/wttr.in/location" | tr -d '\n')"
+	if [ -z "$LOCATION" ]; then
+		echo "^c#ff0090^^d^"
+		exit 0
+	fi
+
+	WTTR="$(curl -sf wttr.in/$LOCATION?format=2 | awk '{print $1 " " $2}')"
+
+	if [ ! -z "$WTTR" ]; then
+		echo "^c#d0d0d0^$WTTR^d^"
+		exit 0
+	fi
+}
+
 print_connection () {
 	ETHSTAT="$(cat /sys/class/net/eth0/operstate)"
 	if [ ! -z "$ETHSTAT" ] && [ "$ETHSTAT" = "up" ]; then
@@ -18,6 +41,6 @@ print_date () {
 }
 
 while true; do
-	xsetroot -name " $(print_connection)  $(print_date) "
+	xsetroot -name " $(print_weather)  $(print_connection)  $(print_date) "
 	sleep 1
 done
