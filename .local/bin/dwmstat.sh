0e20a5d74d62fe9e50a7dea905b4efd84970542d
Tommy Buado <tommy@thinkpad.local>
2023-07-06 23:16:03 +0800

    Add print_connection function in dwmstat.sh script

--- .local/bin/dwmstat.sh
+++ .local/bin/dwmstat.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
+print_connection () {
+	ETHSTAT="$(cat /sys/class/net/eth0/operstate)"
+	if [ ! -z "$ETHSTAT" ] && [ "$ETHSTAT" = "up" ]; then
+		echo "^c#80ff00^^d^"
+		exit 0
+	fi
+
+	WLANSTAT="$(cat /sys/class/net/wlan0/operstate)"
+	if [ ! -z "$WLANSTAT" ] && [ "$WLANSTAT" = "up" ]; then
+		echo "^c#80ff00^^d^"
+		exit 0
+	fi
+}
+
 print_date () {
 	echo "^c#bb88dd^^d^ $(date '+%a %b %d %I:%M %p')"
 }
 
 while true; do
-	xsetroot -name " $(print_date) "
+	xsetroot -name " $(print_connection)  $(print_date) "
 	sleep 1
 done
