221908db342687e4e82ae2608766c9e8e7edf147
Tommy Buado <tommy@thinkpad.local>
2023-07-06 18:05:42 +0800

    Add picom and redshift to startup applications in dwm/autostart.sh script

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+picom --config $HOME/.config/picom/picom.conf &
+redshift -c $HOME/.config/redshift/redshift.conf &
+
 pgrep -x dwmstat.sh > /dev/null
 if [ "$?" -ne 0 ]; then
 	dwmstat.sh 2>&1 > /dev/null &
