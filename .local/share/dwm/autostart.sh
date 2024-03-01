94c8641d1e2e4d66ab007afc4d052970742a388f
Tommy Buado <tommy@thinkpad.local>
2024-03-02 01:18:42 +0800

    Add dwmstat.sh script to start applications

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 picom --config $HOME/.config/picom/picom.conf &
 redshift -c $HOME/.config/redshift/redshift.conf &
+
+pgrep -x dwmstat.sh > /dev/null
+if [ "$?" -ne 0 ]; then
+	$HOME/.local/bin/dwmstat.sh 2>&1 > /dev/null &
+fi
+
 /usr/libexec/pipewire-launcher
