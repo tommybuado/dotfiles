c2a34d56b93a56ad6a8d2416d333bb826d1a76fb
Tommy Buado <tommy@thinkpad.local>
2024-07-02 23:39:51 +0800

    Update display-setting.sh script to include update of wallpapers

--- .local/bin/display-setting.sh
+++ .local/bin/display-setting.sh
@@ -5,26 +5,23 @@ if [ "$MONITOR" = "DUAL" ]; then
 	POSITION="$(echo -e "Mirror\nLeft\nRight" | dmenu $@)"
 	if [ "$POSITION" = "Mirror" ]; then
 		xrandr --output HDMI-2 --auto --same-as eDP-1 --auto
-		exit 0
 	fi
 
 	if [ "$POSITION" = "Left" ]; then
 		xrandr --output HDMI-2 --auto --left-of eDP-1 --auto
-		exit 0
 	fi
 
 	if [ "$POSITION" = "Right" ]; then
 		xrandr --output HDMI-2 --auto --right-of eDP-1 --auto
-		exit 0
 	fi
 fi
 
 if [ "$MONITOR" = "External" ]; then
 	xrandr --output HDMI-2 --auto --output eDP-1 --off
-	exit 0
 fi
 
 if [ "$MONITOR" = "Default" ]; then
 	xrandr --output eDP-1 --auto --output HDMI-2 --off
-	exit 0
 fi
+
+feh --randomize --bg-scale $HOME/.local/share/wallpapers
